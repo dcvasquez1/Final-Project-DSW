6b660879e97517e315b3c94706b2484771c5ac72
dcvasquez1 <dcvasquez@outlook.com>
2018-04-17 09:36:16 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -20,104 +20,79 @@ oauth = OAuth(app)
 
 #Set up Github as OAuth provider
 github = oauth.remote_app(
-    'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], #webapp's pseudo username for github OAuth
-    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'], #webapp's pseudo password for github OAuth
-    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
-    base_url='https://api.github.com/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',  
-    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+	'github',
+	consumer_key=os.environ['GITHUB_CLIENT_ID'], #webapp's pseudo username for github OAuth
+	consumer_secret=os.environ['GITHUB_CLIENT_SECRET'], #webapp's pseudo password for github OAuth
+	request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+	base_url='https://api.github.com/',
+	request_token_url=None,
+	access_token_method='POST',
+	access_token_url='https://github.com/login/oauth/access_token',  
+	authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
 #context processors run before template are rendered and add variable(s) to the template
 #context processors must return a dictionary
 @app.context_processor
 def inject_logged_in():
-    return {"logged_in":('github_token' in session)}
+	return {"logged_in":('github_token' in session)}
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 
 #redirect to HitHub'[s OAuth page and confirm the callback URL
 @app.route('/login')
 def login():   
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
+	return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/logout')
 def logout():
-    session.clear()
-    return render_template('message.html', message='You were logged out')
+	session.clear()
+	return render_template('message.html', message='You were logged out')
 
 @app.route('/login/authorized')#the route should match the callback URL registered with the OAuth provider
 def authorized():
-    resp = github.authorized_response()
-    if resp is None:
-        session.clear()
-        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
-    else:
-        try:
-            #save user data and set log in message
-            session['github_token'] = (resp['access_token'], '')
-            session['user_data'] = github.get('user').data
-            message = "Your were successfully logged in as " + session['user_data']['login']
-        except:
-            #clear the session and give error message
-            session.clear()
-            message = 'Unable to login. Please try again.'
+	resp = github.authorized_response()
+	if resp is None:
+		session.clear()
+		message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+	else:
+		try:
+            		#save user data and set log in message
+			session['github_token'] = (resp['access_token'], '')
+			session['user_data'] = github.get('user').data
+    			message = "Your were successfully logged in as " + session['user_data']['login']
+		except:
+            		#clear the session and give error message
+            		session.clear()
+            		message = 'Unable to login. Please try again.'
             
-    return render_template('message.html', message=message)
+    		return render_template('message.html', message=message)
 
 
 @app.route('/scoreboard')
 def renderScoreboard():
-    return render_template('scoreboard.html')
+	return render_template('scoreboard.html')
 
 @app.route('/clientProfile')
 def renderClientProfile():
-    return render_template('clientProfile.html')
+    	return render_template('clientProfile.html')
 
 @app.route('/gamePage')
 def renderGamePage():
-    return render_template('gamePage.html')
-
-@app.route('/countdown3')
-def countdown3():
-	return Markup('<p>Game Starting in: 3 seconds</p>')
-
-@app.route('/countdown2')
-def countdown2():
-	now = time.time()
-	endTime = now + 1
-	if time.time() > endTime:
-		return Markup('<p>Game Starting in: 2 seconds</p>')
-
-@app.route('/countdown1')
-def countdown1():
-	now = time.time()
-	endTime = now + 2
-	if time.time() > endTime:
-		return Markup('<p>Game Starting in: 1 second</p>')
-
-@app.route('/startGame')
-def startGame():
-	now = time.time()
-	endTime = now + 3
-	if time.time() > endTime:
-		return Markup('<p>Go!</p>')
+	return render_template('gamePage.html')
 
 @app.route('/response')
 def textComplete():
-	return Markup('<p>Recieved Message</p>')
+	return render_template('gamePage.html')
 	
 #the tokengetter is automatically called to check who is logged in
 @github.tokengetter
 def get_github_oauth_token():
-    return session.get('github_token')
+	return session.get('github_token')
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
