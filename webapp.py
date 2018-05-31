ce5fb5621908f24004782984070c4bea4872cecb
dcvasquez1 <dcvasquez@outlook.com>
2018-05-31 09:05:31 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -65,7 +65,7 @@ def scores_to_html():
 
 def createLeaderboard():
     try:
-        tableString = '<table id="rankingTable" cellpadding="5"> <tr> <th style="text-align:left;padding-right: 300px;"></th> <th><u> Play Count </u></th> <th><u> Performance </u></th> <th><u> WPM </u></th> <th><u> Accuracy </u></th> <span style="padding-right:28px; padding-left:28px;"><th><u> S </u></th> <th><u> A </u></th> <th><u> B </u></th> </span> </tr>'
+        tableString = '<table id="rankingTable" cellpadding="5"> <tr> <th style="text-align:left;padding-right: 300px;"></th> <th><u> Play Count </u></th> <th><u> Performance </u></th> <th><u> WPM </u></th> <th><u> Accuracy </u></th> <th style="padding-right:28px; padding-left:28px;"><u> S </u></th> <th style="padding-right:28px; padding-left:28px;"><u> A </u></th> <th style="padding-right:28px; padding-left:28px;"><u> B </u></th> </tr>'
         client = pymongo.MongoClient("mongodb://test_user:18s9h64735f124g5e68@ds247449.mlab.com:47449/dsw-final-project")
         database = client["dsw-final-project"]
         rankingData = database["rankingData"]
